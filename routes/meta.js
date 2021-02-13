97a0b0d011fa275fd35732654ae13d3d5816107b
VitoriaRaquel <viihraquel11@gmail.com>
2021-02-12 22:33:18 -0300

    Update e delete

--- routes/meta.js
+++ routes/meta.js
@@ -45,5 +45,55 @@ router.post("/meta/nova",(req, res)=>{
     }
    
 })
- 
+router.get("/index/edit/:id",(req, res)=>{
+    Metabd.findOne({_id:req.params.id}).lean().then((index) =>{
+        res.render("meta/editmetas",{index: index})
+    }).catch((err)=>{
+        req.flash("error_msg", "Esta meta não existe")
+        res.redirect("/meta/index")
+    })
+   
+})
+router.post("/index/edit", (req, res) => {
+
+    var erros = []
+
+    if(!req.body.titulo || typeof req.body.titulo == undefined || req.body.titulo == null){
+        erros.push({texto:"Nome inválido"})
+
+    }
+    if(erros.length >0){
+        res.render("meta/inserir",{erros: erros})
+    }else{
+
+    Metabd.findOne({_id: req.body.id}).then((index) => {
+
+        index.titulo = req.body.titulo
+        index.descricao = req.body.descricao
+
+        index.save().then(()=>{
+            req.flash("success_msg", "Meta editada com sucesso")
+            res.redirect("/meta")
+        }).catch((err)=>{
+            req.flash("error_msg", "Houve um erro interno ao salvar a meta")
+            res.redirect("/meta")
+        })
+
+    }).catch((err) =>{
+        req.flash("error_msg", "Houve um erro ao editar a categoria")
+        res.redirect("/meta")
+    })
+}
+})
+
+router.post("/index/deletar", (req, res) =>{
+    Metabd.remove({_id: req.body.id}).then(()=>{
+        req.flash("success_msg", "Deletado com sucesso")
+        res.redirect("/meta")
+    }).catch((err) =>{
+        req.flash("error_msg", "Erro ao deletar")
+        res.redirect("/meta")
+    })
+})
+
 module.exports = router
